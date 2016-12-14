07103a9d5d1467d719dfc1d1c9d09d9bed3f95f1
Daniel Ochoa John <dochoajohn@gmail.com>
2016-12-14 16:02:34 -0300

    Add props to button component

--- src/controls/Button.js
+++ src/controls/Button.js
@@ -3,7 +3,7 @@ import React, { PureComponent } from 'react';
 export default class Button extends PureComponent {
   render() {
     return (
-      <a href="#" className="btn btn-primary">
+      <a href={this.props.href} className="btn btn-primary">
         { this.props.label }
       </a>
     );
