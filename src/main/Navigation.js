d708853800a8d588743203647ab7f98556a61297
Daniel Ochoa John <dochoajohn@gmail.com>
2016-12-19 16:25:43 -0300

    Add navigation options prop

--- src/main/Navigation.js
+++ src/main/Navigation.js
@@ -20,18 +20,7 @@ const Navigation = props =>
           {props.children}
         </ul>
 
-        <ul className="nav navbar-nav navbar-right">
-          <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Perfil <span className="caret" /></a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" className="divider" />
-              <li><a href="#">Separated link</a></li>
-            </ul>
-          </li>
-        </ul>
+        {props.navigationOptions}
       </div>
     </div>
   </div>
