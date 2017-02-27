6ff4f763785406cdf6847ac119dbc7b5c4f598d3
Cristian Ferrari <cristianferrarig@gmail.com>
2017-02-27 11:36:55 -0300

    Some adjustments

--- src/main/Section/Section.js
+++ src/main/Section/Section.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Controls from 'main/Controls'
 import childrenPropType from 'propTypes/children'
 
 const Section = props =>
@@ -9,6 +10,16 @@ const Section = props =>
       </div>
     </header>
     <section className="section-content" id="block-content">
+      <div className="section-subhead">
+        <div className="container-max">
+          <Controls
+            main={<a href="#"><i className="fa fa-filter" /><span className="ml-xs hidden-xs">Ver filtros</span></a>}
+            content={<span className="type-subhead">Hay <strong>356</strong> resultados</span>}
+            actions={<a href="#"><span className="mr-xs hidden-xs">Descargar listado</span><i className="fa fa-download" /></a>}
+          />
+        </div>
+      </div>
+
       <div className="container-max">
         {props.content}
       </div>
--- src/shared/ActionPanel/ActionPanel.js
+++ src/shared/ActionPanel/ActionPanel.js
@@ -4,9 +4,12 @@ import childrenPropType from 'propTypes/children'
 const ActionPanel = props =>
   <div className="flex-container flex-vertical">
     <div className="flex-content scrollable">
+      <div className="flex-close">
+        <a href="#"><i className="fa fa-close" /><span className="ml-xs hidden-xs">Cerrar filtros</span></a>
+      </div>
       {props.content}
     </div>
-    <div className="py-sm">
+    <div className="pt-xs pr-sm pb-md pl-sm fade-bottom-base">
       {props.action}
     </div>
   </div>
