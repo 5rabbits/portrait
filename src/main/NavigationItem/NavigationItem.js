5c35b8992cbbc7e2e9f6ad9ac3a42eee5bea24af
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-03-01 14:50:05 -0300

    Supports dropdowns

--- src/main/NavigationItem/NavigationItem.js
+++ src/main/NavigationItem/NavigationItem.js
@@ -2,25 +2,46 @@ import React, { PropTypes } from 'react'
 import classNames from 'classnames'
 
 const NavigationItem = props => {
-  const { component: Component, isActive, icon, label, ...other } = props
+  const { component: Component, isActive, icon, label, children, ...other } = props
+  const isDropdown = children != null
 
   return (
-    <li className={classNames({ active: isActive })}>
-      <Component {...other}>
+    <li
+      className={classNames({
+        active: isActive,
+        dropdown: isDropdown,
+      })}
+      >
+      <Component
+        {...other}
+        data-toggle={isDropdown ? 'dropdown' : undefined}
+        >
         {icon &&
           <span className="mr-xs navigation-item__icon">
             {icon}
           </span>
         }
+
         <span className="hidden-sm">
           {label}
         </span>
+
+        {isDropdown &&
+          <span className="caret navigation-item__caret" />
+        }
       </Component>
+
+      {isDropdown &&
+        <ul className="dropdown-menu">
+          {children}
+        </ul>
+      }
     </li>
   )
 }
 
 NavigationItem.propTypes = {
+  children: PropTypes.node,
   component: PropTypes.node,
   isActive: PropTypes.bool,
   icon: PropTypes.node,
