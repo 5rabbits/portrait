2c4cd78599f8fd9fa7a535b52bce2361849d07ff
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-03-01 15:53:52 -0300

    Allows to specify a className for the navigation

--- src/main/Navigation/Navigation.js
+++ src/main/Navigation/Navigation.js
@@ -3,7 +3,11 @@ import childrenPropType from 'propTypes/children'
 import classNames from 'classnames'
 
 const Navigation = props =>
-  <div className={`navbar navbar-fixed-top navbar-${props.theme}`}>
+  <div
+    className={classNames('navbar', 'navbar-fixed-top',
+      `navbar-${props.theme}`, props.className,
+    )}
+    >
     <div className="container-max">
       <div className="navbar-header">
         <button type="button" className="navbar-toggle collapsed pull-left" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
@@ -38,6 +42,7 @@ Navigation.propTypes = {
   align: PropTypes.oneOf(['left', 'center']).isRequired,
   brand: PropTypes.node,
   children: childrenPropType,
+  className: PropTypes.string,
   navigationOptions: childrenPropType,
   theme: PropTypes.oneOf(['default', 'inverse']).isRequired,
 }
