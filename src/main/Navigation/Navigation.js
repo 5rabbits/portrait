2987025e2edba8dc2204ae4ce588483fe730559f
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-03-03 11:53:15 -0300

    Removes comments

--- src/main/Navigation/Navigation.js
+++ src/main/Navigation/Navigation.js
@@ -10,7 +10,6 @@ class Navigation extends Component {
     brand: PropTypes.node,
     children: childrenPropType,
     className: PropTypes.string,
-    // navigationOptions: childrenPropType,
     theme: PropTypes.oneOf(['default', 'inverse']).isRequired,
     hideTexts: PropTypes.bool,
     collapsed: PropTypes.bool,
@@ -107,36 +106,3 @@ const rules = () => ({
 })
 
 export default responsive(rules)(Navigation)
-
-  // <div
-  //   className={classNames('navbar', 'navbar-fixed-top',
-  //     `navbar-${props.theme}`, props.className,
-  //   )}
-  //   >
-  //   <div className="container-max">
-  //     <div className="navbar-header">
-  //       <button type="button" className="navbar-t
-  // oggle collapsed pull-left" data-toggle="collapse" d
-  // ata-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-  //         <span className="sr-only">Toggle navigation</span>
-  //         <i className="fa fa-bars" />
-  //       </button>
-  //       {props.brand &&
-  //         <div className="navbar-brand">
-  //           {props.brand}
-  //         </div>
-  //       }
-  //     </div>
-  //
-  //     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-  //       <ul
-  //         className={classNames('nav navbar-nav', {
-  //           'navbar-nav--align-left': props.align === 'left',
-  //         })}
-  //         >
-  //         {props.children}
-  //       </ul>
-  //       {props.navigationOptions}
-  //     </div>
-  //   </div>
-  // </div>
