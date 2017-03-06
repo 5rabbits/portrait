341b404207015d14fe1c63402cc981c620a5c233
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-03-06 18:32:16 -0300

    Building components menu

--- docs/containers/App.js
+++ docs/containers/App.js
@@ -2,6 +2,28 @@ import React, { PropTypes } from 'react'
 import { Layout, Navigation, NavigationItem } from 'main'
 import { Link } from 'react-router'
 
+const COMPONENTS = [
+  'controls/RoundButton',
+  'controls/SearchForm',
+  'elements/BlockItem',
+  'main/Controls',
+  'main/Layout',
+  'main/Navigation',
+  'main/NavigationItem',
+  'main/Section',
+  'main/SectionHeading',
+  'main/SectionOffscreen',
+  'shared/Container',
+  'shared/EmptyBlock',
+  'shared/EmptyView',
+  'shared/Grid',
+  'shared/Loader',
+  'shared/Offscreen',
+  'shared/States',
+  'shared/ActionPanel',
+  'templates/CommonSection',
+]
+
 const App = ({ children, router }) =>
   <Layout
     navigation={
@@ -17,12 +39,15 @@ const App = ({ children, router }) =>
           isActive={router.isActive('/components')}
           label="Components"
           >
-          <NavigationItem
-            component={Link}
-            label="Navigation"
-            to="/components/main/Navigation"
-            isActive={router.isActive('/components/main/Navigation', true)}
-          />
+          {COMPONENTS.map((component, index) =>
+            <NavigationItem
+              component={Link}
+              key={index}
+              label={component.split('/').pop()}
+              to={`/components/${component}`}
+              isActive={router.isActive(`/components/${component}`, true)}
+            />,
+          )}
         </NavigationItem>
 
         <NavigationItem
