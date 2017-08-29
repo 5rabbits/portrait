ac21064582767e1d88ed9231c94af285ca514959
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-08-29 10:35:35 -0300

    Allow falsy values

--- src/components/controls/Select/Select.js
+++ src/components/controls/Select/Select.js
@@ -103,7 +103,7 @@ const defaultInputRenderer = ({
       type="text"
       value={value}
     />
-    {canCreate && value && !hasResults &&
+    {canCreate && value != null && !hasResults &&
       <button
         className="Select__input__icon Select__input__icon--clickable"
         onClick={onNewOptionClick}
@@ -115,7 +115,7 @@ const defaultInputRenderer = ({
         </svg>
       </button>
     }
-    {(!canCreate || !value || hasResults) &&
+    {(!canCreate || value == null || hasResults) &&
       <div className="Select__input__icon">
         <svg viewBox="0 0 400 400">
           <path d="M292.188 243.75c15.625-24.583 24.79-53.75 24.79-85.104C316.98 71.042 246.043 0 158.543 0 70.937 0 0 71.042 0 158.646c0 87.604 70.938 158.646 158.438 158.646 31.77 0 61.354-9.375 86.145-25.417l7.188-5L364.897 400 400 364.27 286.98 251.147l5.207-7.396zM247.29 70c23.645 23.646 36.666 55.104 36.666 88.542 0 33.437-13.02 64.895-36.666 88.54-23.646 23.647-55.105 36.668-88.542 36.668-33.438 0-64.896-13.02-88.542-36.667-23.645-23.645-36.666-55.104-36.666-88.54 0-33.44 13.02-64.897 36.666-88.543 23.646-23.646 55.105-36.667 88.542-36.667 33.438 0 64.896 13.02 88.542 36.667z" />
@@ -388,7 +388,7 @@ export default class Select extends PureComponent {
   }
 
   getSelectedOption() {
-    if (!this.state.value) {
+    if (this.state.value == null) {
       return null
     }
 
