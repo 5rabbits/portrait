c82a826ea22996c398e396444a9b5701e3811ad9
Rodrigo Díaz <rdiazv89@gmail.com>
2017-08-31 15:25:30 -0300

    Adds the classnames lib to examples

--- docs/components/Example.js
+++ docs/components/Example.js
@@ -4,6 +4,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
 import Playground from 'component-playground'
+import classNames from 'classnames'
 import markdown from '../helpers/markdown'
 
 const Example = ({ code, collapsible, componentName, description, expanded, inline }) =>
@@ -17,7 +18,7 @@ const Example = ({ code, collapsible, componentName, description, expanded, inli
       collapsableCode={collapsible}
       initiallyExpanded={expanded}
       noRender={inline}
-      scope={{ React, ReactDOM }}
+      scope={{ React, ReactDOM, classNames }}
       theme="material"
     />
   </div>
